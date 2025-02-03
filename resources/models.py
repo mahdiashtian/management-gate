a14bdb120bcf9a0fdd335dc42f088feacf0368d0
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2025-02-03 20:18:15 +0330

    feat: change resource model

--- resources/models.py
+++ resources/models.py
@@ -12,9 +12,27 @@ class ColorChoices(models.TextChoices):
     blue = 'blue', 'blue'
 
 
+class StatusChoices(models.TextChoices):
+    accept = 'accept', 'accept'
+    pending = 'pending', 'pending'
+    reject = 'reject', 'reject'
+
+
+class TypeChoices(models.TextChoices):
+    citizen = 'citizen', 'citizen'
+
+
+class LicensePlate(models.Model):
+    section_1 = models.CharField(max_length=12, null=True, blank=True, verbose_name='بخش اول')
+    section_2 = models.CharField(max_length=12, null=True, blank=True, verbose_name='بخش دوم')
+    section_3 = models.CharField(max_length=12, null=True, blank=True, verbose_name='بخش سوم')
+    section_4 = models.CharField(max_length=12, null=True, blank=True, verbose_name='بخش چهارم')
+    type = models.CharField(max_length=10, choices=TypeChoices, verbose_name='مدل')
+
+
 class Cars(models.Model):
     owner = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='مالک')
-    license_plate = models.CharField(max_length=15, unique=True, verbose_name='شماره پلاک')
+    license_plate = models.OneToOneField(LicensePlate, on_delete=models.CASCADE)
     model = models.CharField(choices=ModelChoices.choices, default=ModelChoices.peugeot_405, verbose_name='مدل',
                              max_length=20)
     color = models.CharField(choices=ColorChoices.choices, default=ColorChoices.blue, verbose_name='رنگ', max_length=10)
@@ -29,6 +47,7 @@ class EntryPermits(BaseModel):
     entry_count = models.PositiveIntegerField(default=10, verbose_name='تعداد دفعات مجاز')
     expiration = models.DateTimeField(verbose_name='تاریخ انقضا')
     notes = models.TextField(blank=True, verbose_name='توضیحات')
+    status = models.CharField(max_length=10, choices=StatusChoices.choices, verbose_name='وضعیت')
 
     class Meta:
         verbose_name = "مجوز"
