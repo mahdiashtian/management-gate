42d257a9fbbc43f92f28f2bf5edc7528c520a2eb
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2025-02-10 18:03:24 +0330

    feat: add cors

--- config/settings.py
+++ config/settings.py
@@ -43,12 +43,15 @@ INSTALLED_APPS = [
     'core',
     'users',
     'resources',
-    'logs'
+    'logs',
+
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -143,3 +146,11 @@ REST_FRAMEWORK = {
     ],
     'PAGE_SIZE': 11,
 }
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "https://localhost:8080",
+    "https://127.0.0.1:8080",
+]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
